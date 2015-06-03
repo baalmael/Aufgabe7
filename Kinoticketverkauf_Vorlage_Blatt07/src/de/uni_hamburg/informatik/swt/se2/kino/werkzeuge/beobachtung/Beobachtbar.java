1ab5c2085e981d0b53ead8a82704d6fed5b6cb1f
Elias <eliasaaronbaalmann@gmx.de>
2015-06-03 16:16:14 +0200

    Hinzufügen der Kommentare

--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/beobachtung/Beobachtbar.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/beobachtung/Beobachtbar.java
@@ -2,25 +2,49 @@ package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.beobachtung;
 
 import java.util.HashSet;
 
+/**
+ * Die zu beobachtenden Klassen können von dieser Klasse erben, 
+ * sie stellt die Verwaltung und Benachrichtigung der Beobachter bereit.
+ */
 public abstract class Beobachtbar
 {
+    /**
+     * Die Menge der registrierten Beobachter.
+     */
     private HashSet<Beobachter> _beobachterSet;
 
+    /**
+     * Initialisiert einen neuen abstrakten Beobachter.
+     */
     public Beobachtbar()
     {
         _beobachterSet = new HashSet<Beobachter>();
     }
 
+    /**
+     * Meldet den gegebenen Beoabachter an.
+     * 
+     * @param beobachter Ein Beobachter, der angemeldet werden soll.
+     */
     public void registriereBeobachter(Beobachter beobachter)
     {
         _beobachterSet.add(beobachter);
     }
 
+    /**
+     * Meldet den gegebenen Beoabachter ab.
+     * 
+     * @param beobachter Ein Beobachter, der abgemeldet werden soll.
+     */
     public void entferneBeobachter(Beobachter beobachter)
     {
         _beobachterSet.remove(beobachter);
     }
 
+    /**
+     * Informiert alle angemeldeten Beobachter dass eine relevante Änderung
+     * eingetreten ist.
+     */
     protected void informiereUeberAenderung()
     {
         for (Beobachter beobachter : _beobachterSet)
--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/beobachtung/Beobachter.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/beobachtung/Beobachter.java
@@ -1,8 +1,20 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.beobachtung;
 
+/**
+ * * Interface für Beobachter, die sich für Änderungen eines Beobachtbaren
+ * interessieren.
+ */
 public interface Beobachter
 {
-
+    /**
+     * Diese Operation wird aufgerufen, sobald sich an an dem beobachteten
+     * Subwerkzeug etwas relevantes geändert hat.
+     * 
+     * Implementierende Klassen müssen in dieser Operation auf die Aenderung
+     * reagieren.
+     * 
+     * @param beobachtbar Der Beobachtbare
+     */
     void reagiereAufAenderung(Beobachtbar beobachtbar);
 
 }
